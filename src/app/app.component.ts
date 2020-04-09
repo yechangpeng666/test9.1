be6dbadbcca8c51b22991b205e46c2d52d9296ed
yechangpeng666 <2201427581@qq.com>
2020-04-09 17:55:53 +0800

    第九周作业

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Product } from './product'
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'invontory-app';
+  products: Product[];
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/imgs/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/imgs/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/imgs/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+    ];
+  }
+  onProductSelected(product: Product) {
+    console.log('在根组件中,响应产品:' + product.name + "选中事件!!")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list.component';
+import { ProductDepartmentComponent } from './product-department.component';
+import { ProductImageComponent } from './product-image.component';
+import { ProductPriceComponent } from './product-price.component';
+import { ProductRowComponent } from './product-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductDepartmentComponent,
+    ProductImageComponent,
+    ProductPriceComponent,
+    ProductRowComponent,
   ],
   imports: [
     BrowserModule
